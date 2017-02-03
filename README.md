6b724c473e10fc0a3f31fdcd1187078e8637f0c9
Natasha Kaweski <naweskil@gmail.com>
2017-02-02 22:12:48 -0200

    Updating some features

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# HTML Skeleton ([Gulp](http://gulpjs.com) version)
+# HTML Skeleton ([Gulp.js](http://gulpjs.com) version)
 
-Base structure for front end project together with Gulpjs
+Base structure for front-end project together with Gulp.js
 
 ## Quick start
 
@@ -11,44 +11,11 @@ Base structure for front end project together with Gulpjs
 ## Features
 
 * Gulp ready;
-* Browser sync;
+* CSS made with Less :heart:;
+* Facebook OG Tags;
 * Placeholder crossbrowser ready;
-* Comes with twitter bootstrap;
-
-####Browser Sync with dynamic files
-
-If you want to work with dynamic files like `.php`, it's necessary make some changes on gulpfile.js.
-
-Change the value of `var extension` - Line 28 - to `'php'`.
-
-Remove (or make a comment) on these lines:
-
-```
-70	server: {
-71		baseDir: "./"
-72	}
-
-...
-
-109	server: {
-110		baseDir: "./"
-111	}
-```
-
-Uncomment the lines below:
-
-```
-74	//proxy: "localhost/html-skeleton-gulp/"
-
-...
-
-113 //proxy: "localhost/html-skeleton-gulp/"
-```
-
-*Note: You must have an apache server running (like WAMP, MAMP, LAMP or native). Declare as the value of the proxy, the directory of your project folder located in the files of apache.*
+* Comes with twitter bootstrap.
 
 ## Follow-me
 * [Twitter](https://twitter.com/samwebdesign)
 * [Google Plus](https://plus.google.com/+SamuelMartins/posts)
-
-## Rest of description soon
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,29 @@
 // Load gulp plugins with 'require' function of nodejs
-var gulp        = require('gulp'),
-	plumber     = require('gulp-plumber'),
-	gutil       = require('gulp-util'),
-	uglify      = require('gulp-uglify'),
-	concat      = require('gulp-concat'),
-	rename      = require('gulp-rename'),
-	minifyCSS   = require('gulp-minify-css'),
-	less        = require('gulp-less'),
-	path        = require('path'),
-	livereload  = require('gulp-livereload'),
-	browserSync = require('browser-sync');
+var gulp    = require('gulp'),
+plumber     = require('gulp-plumber'),
+gutil       = require('gulp-util'),
+uglify      = require('gulp-uglify'),
+concat      = require('gulp-concat'),
+rename      = require('gulp-rename'),
+minifyCSS   = require('gulp-minify-css'),
+less        = require('gulp-less'),
+path        = require('path');
 
 // Handle less error
 var onError = function (err) {
-	gutil.beep();
-	console.log(err);
+  gutil.beep();
+  console.log(err);
 };
 
 // Path configs
-var css_files  = 'assets/css/*.css', // .css files
-	css_path   = 'assets/css', // .css path
-	js_files   = 'assets/js/*.js', // .js files
-	less_file  = 'assets/less/style.less', // .less files
-	dist_path  = 'assets/dist';
+var appPathSrc = 'assets';
+
+var css_files = appPathSrc + '/css/**/*.css', // .css files
+    css_path  = appPathSrc + '/css', // .css path
+    js_files  = appPathSrc + '/js/**/*.js', // .js files
+    less_file = appPathSrc + '/less/style.less', // .less files
+    less_path = appPathSrc + '/less/**/*.less',
+    dist_path = appPathSrc + '/dist';
 
 //Extension config
 var extension = 'html';
@@ -30,112 +31,62 @@ var extension = 'html';
 
 /***** Functions for tasks *****/
 function js() {
-	return gulp.src(js_files)
-			.pipe(plumber({
-				errorHandler: onError
-			}))
-			.pipe(concat('dist'))
-			.pipe(rename('concat.min.js'))
-			.pipe(uglify())
-			.pipe(gulp.dest(dist_path))
-			.pipe(livereload());
+  return gulp.src(js_files)
+      .pipe(plumber({
+        errorHandler: onError
+      }))
+      .pipe(concat('dist'))
+      .pipe(rename('app.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(dist_path));
 }
 
 function css() {
-	return gulp.src(css_files)
-			.pipe(concat('dist'))
-			.pipe(rename('all.min.css'))
-			.pipe(minifyCSS({keepBreaks:true}))
-			.pipe(gulp.dest(dist_path))
-			.pipe(livereload());
+  return gulp.src(css_files)
+      .pipe(concat('dist'))
+      .pipe(rename('all.min.css'))
+      .pipe(minifyCSS({keepBreaks:false, keepSpecialComments: false}))
+      .pipe(gulp.dest(dist_path));
 }
 
 function lessTask(err) {
-	return gulp.src(less_file)
-			.pipe(plumber({
-				errorHandler: onError
-			}))
-			.pipe(less({ paths: [ path.join(__dirname, 'less', 'includes') ] }))
-			.pipe(gulp.dest(css_path))
-			.pipe(livereload());
+  return gulp.src(less_file)
+      .pipe(plumber({
+        errorHandler: onError
+      }))
+      .pipe(less({ paths: [ path.join(__dirname, 'less', 'includes') ] }))
+      .pipe(gulp.dest(css_path));
 }
-
-function reloadBrowser() {
-	return gulp.src('*.' + extension)
-			.pipe(livereload());
-}
-
-function browserSync() {
-	return browserSync({
-		server: {
-			baseDir: "./"
-		}
-
-		//proxy: "localhost/html-skeleton-gulp/"
-	});
-}
-
-// The 'Browser Sync' task
-gulp.task('browser-sync', function() {
-	return browserSync();
-});
 
 // The 'js' task
 gulp.task('js', function() {
-	return js();
+  return js();
 });
 
 // The 'css' task
 gulp.task('css', function(){
-	return css();
+  return css();
 });
 
 // The 'less' task
 gulp.task('less', function(){
-	return lessTask();
+  return lessTask();
 });
 
-// Reload browser when have *.html changes
-gulp.task('reload-browser', function() {
-	return reloadBrowser();
-});
 
 // The 'default' task.
 gulp.task('default', function() {
-	livereload.listen();
-	//browserSync();
+  gulp.watch(less_path, function() {
+    return lessTask();
+  });
 
-	browserSync({
-		server: {
-			baseDir: "./"
-		}
+  gulp.watch(css_files, function() {
+    console.log('CSS task completed!');
+    return css();
+  });
 
-		//proxy: "localhost/html-skeleton-gulp/"
-	});
-
-	gulp.watch('*.' + extension, function(){
-		browserSync.reload();
-	});
-
-	gulp.watch(less_file, function() {
-		// if (err) return console.log(err);
-		return lessTask();
-	});
-
-	gulp.watch(css_files, function() {
-		console.log('CSS task completed!');
-		browserSync.reload();
-		return css();
-	});
-
-	gulp.watch(js_files, function() {
-		console.log('JS task completed!');
-		browserSync.reload();
-		return js();
-	});
-
-	gulp.watch('*.' + extension, function(){
-		console.log('Browse reloaded!');
-		return reloadBrowser();
-	});
+  gulp.watch(js_files, function() {
+    console.log('JS task completed!');
+    return js();
+  });
 });
